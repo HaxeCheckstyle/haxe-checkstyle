bbc5e748cc8144eec0cfb46b043b8e0139c9ad49
Adi Mora <adi.mora@gamesys.co.uk>
2015-04-06 20:08:36 +0100

    added indentation character check test

---
+++ test/IndentationCharacterCheckTest.hx
@@ -0,0 +1,43 @@
+package ;
+
+import checkstyle.checks.IndentationCharacterCheck;
+
+class IndentationCharacterCheckTest extends CheckTestCase {
+
+	public function testWrongIndentation() {
+		var msg = checkMessage(IndentationTests.TEST1, new IndentationCharacterCheck());
+		assertEquals(msg, 'Wrong indentation character (tab)');
+	}
+
+	public function testCorrectIndentation() {
+		var msg = checkMessage(IndentationTests.TEST2, new IndentationCharacterCheck());
+		assertEquals(msg, '');
+	}
+
+	public function testConfigurableIndentation() {
+		var check = new IndentationCharacterCheck();
+		check.character = "space";
+
+		var msg = checkMessage(IndentationTests.TEST3, check);
+		assertEquals(msg, 'Wrong indentation character (space)');
+	}
+}
+
+class IndentationTests {
+	public static inline var TEST1:String = "
+	class Test {
+		 var _a:Int;
+		public function new() {}
+	}";
+
+	public static inline var TEST2:String =
+	"class Test {
+		var _a:Int;
+		public function new() {}
+	}";
+
+	public static inline var TEST3:String =
+	"class Test {
+		public function new() {}
+	}";
+}
