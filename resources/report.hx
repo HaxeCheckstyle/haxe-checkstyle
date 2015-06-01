99a4ee9541024654031d817f67d351b73394a6f2
Adi Mora <adi.mora@gamesys.co.uk>
2015-06-01 23:30:09 +0200

    added reporting utility

--- resources/report.hx
+++ resources/report.hx
@@ -1,15 +1,39 @@
 package ;
 
+import haxe.CallStack;
+import hxargs.Args;
 import sys.io.FileOutput;
 import sys.io.File;
 
-class StaticAnalysis {
+class Report {
 
 	var _staticAnalysisXML:Xml;
 	var _reportFile:FileOutput;
 
+	var _path:String;
+
+	function run(args:Array<String>) {
+		var files:Array<String> = [];
+		var configPath:String = null;
+
+		var argHandler = Args.generate([
+			@doc("xml path") ["-p", "--path"] => function(loc:String) _path = loc,
+			_ => function(arg:String) throw "Unknown command: " + arg
+		]);
+
+		if (args.length > 0) {
+			argHandler.parse(args);
+		}
+
+		generateReport();
+	}
+
 	public function new() {
-		_staticAnalysisXML = Xml.parse(File.getContent("./resources/static-analysis.xml"));
+		_path = "./resources/static-analysis.xml";
+	}
+	
+	function generateReport() {
+		_staticAnalysisXML = Xml.parse(File.getContent(_path));
 		_reportFile = File.write("CHECKS.md", false);
 		_reportFile.writeString("###Report of default checks on CheckStyle library itself\n\n");
 		var errors = 0;
@@ -46,7 +70,28 @@ class StaticAnalysis {
 		_reportFile.writeString("`Total Issues: " + total + " (Errors: " + errors + " Warnings: " + warnings + " Infos: " + infos + ")`");
 	}
 
-	static function main() {
-		new StaticAnalysis();
+	@SuppressWarnings('checkstyle:Dynamic')
+	public static function main() {
+		var args;
+		var cwd;
+		var oldCwd = null;
+
+		try {
+			args = Sys.args();
+			cwd = Sys.getCwd();
+			if (Sys.getEnv("HAXELIB_RUN") != null) {
+				cwd = args.pop();
+				oldCwd = Sys.getCwd();
+			}
+			if (oldCwd != null) Sys.setCwd(cwd);
+
+			var main = new Report();
+			main.run(args);
+		}
+		catch(e:Dynamic) {
+			trace(e);
+			trace(CallStack.toString(CallStack.exceptionStack()));
+		}
+		if (oldCwd != null) Sys.setCwd(oldCwd);
 	}
 }
