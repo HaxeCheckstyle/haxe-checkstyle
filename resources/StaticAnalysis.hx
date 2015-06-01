0cadf5dd1b72242cc4f72e04e4db88a2150d2a00
Adi Mora <adi.mora@gamesys.co.uk>
2015-06-01 22:54:03 +0200

    generating default checks report on check style itself

--- resources/StaticAnalysis.hx
+++ resources/StaticAnalysis.hx
@@ -1,13 +1,17 @@
 package ;
 
+import sys.io.FileOutput;
 import sys.io.File;
 
 class StaticAnalysis {
 
 	var _staticAnalysisXML:Xml;
+	var _reportFile:FileOutput;
 
 	public function new() {
 		_staticAnalysisXML = Xml.parse(File.getContent("./resources/static-analysis.xml"));
+		_reportFile = File.write("CHECKS.md", false);
+		_reportFile.writeString("###Report of default checks on CheckStyle library itself\n\n");
 		var errors = 0;
 		var warnings = 0;
 		var infos = 0;
@@ -23,18 +27,23 @@ class StaticAnalysis {
 					case "error":
 						errors++;
 						Sys.println("\t\033[91mError: LINE - " + errorNode.get("line") + ": " + StringTools.htmlUnescape(errorNode.get("message")) + "\033[0m");
+						_reportFile.writeString("`Error: LINE - " + errorNode.get("line") + ": " + StringTools.htmlUnescape(errorNode.get("message")) + "`\n\n");
 					case "warning":
 						warnings++;
 						Sys.println("\t\033[95mWarning: LINE - " + errorNode.get("line") + ": " + StringTools.htmlUnescape(errorNode.get("message")) + "\033[0m");
+						_reportFile.writeString("`Warning: LINE - " + errorNode.get("line") + ": " + StringTools.htmlUnescape(errorNode.get("message")) + "`\n\n");
 					case "info":
 						infos++;
 						Sys.println("\t\033[94mInfo: LINE - " + errorNode.get("line") + ": " + StringTools.htmlUnescape(errorNode.get("message")) + "\033[0m");
+						_reportFile.writeString("`Info: LINE - " + errorNode.get("line") + ": " + StringTools.htmlUnescape(errorNode.get("message")) + "`\n\n");
 				}
 				total++;
 			}
 		}
 
 		Sys.println("\033[1m\nTotal Issues: " + total + " (\033[0m\033[91mErrors: " + errors + "\033[0m, \033[95mWarnings: " + warnings + "\033[0m, \033[94mInfos: " + infos + ")\n" + "\033[0m");
+
+		_reportFile.writeString("`Total Issues: " + total + " (Errors: " + errors + " Warnings: " + warnings + " Infos: " + infos + ")`");
 	}
 
 	static function main() {
