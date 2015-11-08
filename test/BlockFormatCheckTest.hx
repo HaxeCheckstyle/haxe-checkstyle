3f2b4eb3157c0e8b7a5f861adf55a8d588978c39
Adi Mora <adi.mora@gamesys.co.uk>
2015-11-08 00:15:52 +0000

    updated tests

--- test/BlockFormatCheckTest.hx
+++ test/BlockFormatCheckTest.hx
@@ -23,13 +23,6 @@ class BlockFormatCheckTest extends CheckTestCase {
 		var msg = checkMessage(BlockFormatTests.TEST3, new BlockFormatCheck());
 		assertEquals(msg, 'Last line of multiline block should contain only } and maybe , or ;');
 	}
-
-	public function testDisabledBlockFormat() {
-		var test = new BlockFormatCheck();
-		test.emptyBlockCheck = false;
-		var msg = checkMessage(BlockFormatTests.TEST4, test);
-		assertEquals(msg, '');
-	}
 }
 
 class BlockFormatTests {
@@ -56,12 +49,5 @@ class BlockFormatTests {
 	"class Test {
 		public function new() {
 			var a:Int; }
-	}";
-
-	public static inline var TEST4:String = "
-	class Test {
-		public function new() {
-		
-		}
 	}";
 }
