b7d054b9be974ee89e0a27b3d1b30cc816cd767e
Adi Mora <adi.mora@gamesys.co.uk>
2015-11-08 00:15:03 +0000

    removed emptyBlockCheck

--- checkstyle/checks/BlockFormatCheck.hx
+++ checkstyle/checks/BlockFormatCheck.hx
@@ -6,14 +6,11 @@ import checkstyle.LintMessage.SeverityLevel;
 @desc("Checks empty blocks and first/last lines of a block")
 class BlockFormatCheck extends Check {
 
-	public var emptyBlockCheck:Bool;
-
 	var firstLineRE:EReg;
 	var lastLineRE:EReg;
 
 	public function new() {
 		super();
-		emptyBlockCheck = true;
 		firstLineRE = ~/\{[\/*]?\s*$/;
 		lastLineRE = ~/^\s*\}[,;\/*]?/;
 	}
@@ -23,7 +20,7 @@ class BlockFormatCheck extends Check {
 			if (isPosSuppressed(e.pos)) return;
 			switch(e.expr){
 				case EBlock([]) | EObjectDecl([]):
-					if (emptyBlockCheck && e.pos.max - e.pos.min > "{}".length) {
+					if (e.pos.max - e.pos.min > "{}".length) {
 						logPos("Empty block should be written as {}", e.pos, Reflect.field(SeverityLevel, severity));
 					}
 				case EBlock(_) | EObjectDecl(_):
