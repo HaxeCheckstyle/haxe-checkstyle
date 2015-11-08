2e1ccbea7b03a0712fa96cdd1a88c342ca0bee9b
Adi Mora <adi.mora@gamesys.co.uk>
2015-11-08 15:12:21 +0000

    added case option for hexadecimal check

--- test/HexadecimalLiteralsCheckTest.hx
+++ test/HexadecimalLiteralsCheckTest.hx
@@ -4,25 +4,37 @@ import checkstyle.checks.HexadecimalLiteralsCheck;
 
 class HexadecimalLiteralsCheckTest extends CheckTestCase {
 
-	public function testCorrectOverride() {
+	public function test1() {
 		var msg = checkMessage(HexadecimalTests.TEST1, new HexadecimalLiteralsCheck());
-		assertEquals(msg, 'Bad hexademical literal, use uppercase');
+		assertEquals(msg, 'Bad hexademical literal, use upperCase');
 	}
 
-	public function testWrongOverride() {
+	public function test2() {
 		var msg = checkMessage(HexadecimalTests.TEST2, new HexadecimalLiteralsCheck());
 		assertEquals(msg, '');
+	}
+
+	public function test3() {
+		var check = new HexadecimalLiteralsCheck();
+		check.option = "lowerCase";
+		var msg = checkMessage(HexadecimalTests.TEST3, check);
+		assertEquals(msg, 'Bad hexademical literal, use lowerCase');
 	}
 }
 
 class HexadecimalTests {
 	public static inline var TEST1:String = "
 	class Test {
-		var _clr = 0xffffff;
+		var clr = 0xffffff;
 	}";
 
 	public static inline var TEST2:String =
 	"class Test {
-		var _clr = 0x0033FF;
+		var clr = 0x0033FF;
+	}";
+
+	public static inline var TEST3:String =
+	"class Test {
+		var clr = 0x0033FF;
 	}";
 }
