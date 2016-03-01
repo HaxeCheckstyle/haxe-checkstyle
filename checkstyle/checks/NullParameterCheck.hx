d9569dd8ec70b98cfad2bf6b4ba52346dc5e9e00
Gama11 <jensfischer95@gmail.com>
2016-03-01 16:55:38 +0100

    Add a nullDefaultValueStyle option to NullParameterCheck

--- checkstyle/checks/NullParameterCheck.hx
+++ checkstyle/checks/NullParameterCheck.hx
@@ -9,19 +9,44 @@ using haxe.macro.ExprTools;
 @desc("Checks for parameters with a null default value")
 class NullParameterCheck extends Check {
 
+	public static inline var EITHER:String = "either";
+	public static inline var QUESTION_MARK:String = "questionmark";
+	public static inline var NULL:String = "null";
+
+	public var nullDefaultValueStyle:String;
+
+	public function new() {
+		super();
+		nullDefaultValueStyle = QUESTION_MARK;
+	}
+
 	override function actualRun() {
 		forEachField(function(field, _) {
 			switch (field.kind) {
 				case FFun(f):
-					for (arg in f.args) checkArgument(arg);
+					for (arg in f.args) checkArgument(arg, field.pos);
 				case _:
 			}
 		});
 	}
 
-	function checkArgument(arg:FunctionArg) {
-		if (arg.opt && arg.value.toString() == "null") {
-			logPos('Parameter ${arg.name} has a \'?\' and a default value of \'null\', which is redundant', arg.value.pos, Reflect.field(SeverityLevel, severity));
+	function checkArgument(arg:FunctionArg, pos:Position) {
+		var hasNullDefault = arg.value.toString() == "null";
+		var prefix = 'Parameter \'${arg.name}\' ';
+		if (arg.opt && hasNullDefault) {
+			logPos(prefix + "is marked as optional with '?' and has a default value of 'null', which is redundant",
+				arg.value.pos, Reflect.field(SeverityLevel, severity));
+		}
+		else {
+			var line = checker.getLinePos(pos.min).line + 1;
+			if (hasNullDefault && nullDefaultValueStyle == QUESTION_MARK) {
+				log(prefix + "should be marked as optional with '?' instead of using a null default value",
+					line, 0, null, Reflect.field(SeverityLevel, severity));
+			}
+			else if (arg.opt && nullDefaultValueStyle == NULL) {
+				log(prefix + "should have a null default value instead of being marked as optional with '?'",
+					line, 0, null, Reflect.field(SeverityLevel, severity));
+			}
 		}
 	}
 }
--- test/NullParameterCheckTest.hx
+++ test/NullParameterCheckTest.hx
@@ -6,15 +6,31 @@ class NullParameterCheckTest extends CheckTestCase {
 
 	public function testDetectRedundancy() {
 		assertMsg(new NullParameterCheck(), NullParameterCheckTests.TEST1,
-			"Parameter a has a '?' and a default value of 'null', which is redundant");
+			"Parameter 'a' is marked as optional with '?' and has a default value of 'null', which is redundant");
 	}
 
-	public function testSuppressRedundancy() {
+	public function testSuppress() {
 		assertMsg(new NullParameterCheck(), NullParameterCheckTests.TEST2, '');
 	}
 	
-	public function testNoRedundancy() {
-		assertMsg(new NullParameterCheck(), NullParameterCheckTests.TEST3, '');
+	public function testAllowEitherStyle() {
+		var check = new NullParameterCheck();
+		check.nullDefaultValueStyle = NullParameterCheck.EITHER;
+		assertMsg(check, NullParameterCheckTests.TEST3, '');
+	}
+
+	public function testPreferQuestionMark() {
+		var check = new NullParameterCheck();
+		check.nullDefaultValueStyle = NullParameterCheck.QUESTION_MARK;
+		assertMsg(check, NullParameterCheckTests.TEST3,
+			"Parameter 'b' should be marked as optional with '?' instead of using a null default value");
+	}
+
+	public function testPreferNull() {
+		var check = new NullParameterCheck();
+		check.nullDefaultValueStyle = NullParameterCheck.NULL;
+		assertMsg(check, NullParameterCheckTests.TEST3,
+			"Parameter 'a' should have a null default value instead of being marked as optional with '?'");
 	}
 }
 
@@ -27,7 +43,7 @@ class NullParameterCheckTests {
 	public static inline var TEST2:String = "
 	abstractAndClass Test {
 		@SuppressWarnings('checkstyle:NullParameter')
-		function test(?a:Int = null) {}
+		function test(?a:Int = null, b:Null<Int> = null, ?c:Int) {}
 	}";
 	
 	public static inline var TEST3:String = "
