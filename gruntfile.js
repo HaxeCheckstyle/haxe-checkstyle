b66298f91d64f0141025f6cb6fc8efb69b68a22a
Adi <adiinteractive@gmail.com>
2016-03-03 09:36:27 +0000

    moved checkstyle package to src folder

--- gruntfile.js
+++ gruntfile.js
@@ -1,20 +1,20 @@
 module.exports = function (grunt) {
 
-    grunt.initConfig({
-         pkg: grunt.file.readJSON("package.json"),
+	grunt.initConfig({
+		pkg: grunt.file.readJSON("package.json"),
 
-         haxe: {
-             project: {
-                 hxml: "build.hxml"
-             }
-         },
+		haxe: {
+			project: {
+				hxml: "build.hxml"
+			}
+		},
 
-         zip: {
-             "checkstyle.zip": ["checkstyle/**", "haxelib.json", "run.n", "README.md"]
-         }
-     });
+		zip: {
+			"checkstyle.zip": ["src/**", "resources/sample-config.json", "haxelib.json", "run.n", "README.md"]
+		}
+	});
 
-    grunt.loadNpmTasks("grunt-haxe");
-    grunt.loadNpmTasks("grunt-zip");
-    grunt.registerTask("default", ["haxe"]);
+	grunt.loadNpmTasks("grunt-haxe");
+	grunt.loadNpmTasks("grunt-zip");
+	grunt.registerTask("default", ["haxe"]);
 };
