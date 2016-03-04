157c8d131cf5359c8f079db325301e2c7ebe85df
Gama11 <jensfischer95@gmail.com>
2016-03-04 16:09:39 +0100

    Report: get rid of some redundant code

--- src/checkstyle/Report.hx
+++ src/checkstyle/Report.hx
@@ -23,21 +23,27 @@ class Report {
 			if (fileNode.elementsNamed("error").hasNext()) Sys.println("\033[1mCLASS: " + fileName + "\033[0m");
 			for (error in fileNode.elementsNamed("error")) {
 				var errorNode:Xml = error;
+				var line = errorNode.get("line");
+				var message = StringTools.htmlUnescape(errorNode.get("message"));
 				switch (errorNode.get("severity")) {
 					case "error":
 						errors++;
-						Sys.println("\t\033[91mError: LINE - " + errorNode.get("line") + ": " + StringTools.htmlUnescape(errorNode.get("message")) + "\033[0m");
+						printMessage(91, "Error", line, message);
 					case "warning":
 						warnings++;
-						Sys.println("\t\033[95mWarning: LINE - " + errorNode.get("line") + ": " + StringTools.htmlUnescape(errorNode.get("message")) + "\033[0m");
+						printMessage(95, "Warning", line, message);
 					case "info":
 						infos++;
-						Sys.println("\t\033[94mInfo: LINE - " + errorNode.get("line") + ": " + StringTools.htmlUnescape(errorNode.get("message")) + "\033[0m");
+						printMessage(94, "Info", line, message);
 				}
 				total++;
 			}
 		}
 
 		Sys.println("\033[1m\nTotal Issues: " + total + " (\033[0m\033[91mErrors: " + errors + "\033[0m, \033[95mWarnings: " + warnings + "\033[0m, \033[94mInfos: " + infos + ")\n" + "\033[0m");
+	}
+
+	function printMessage(color:Int, type:String, line:String, message:String) {
+		Sys.println("\t\033[" + color + "m" + type + ": LINE - " + line + ": " + message + "\033[0m");
 	}
 }
