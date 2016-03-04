f2d071aba4bbabd156549b0f6e33d9cbac2b74bc
Gama11 <jensfischer95@gmail.com>
2016-03-04 17:27:36 +0100

    TypeCheck: ignore enum abstract values by default
    Enum abstract values can't have type hints.

--- src/checkstyle/checks/TypeCheck.hx
+++ src/checkstyle/checks/TypeCheck.hx
@@ -8,9 +8,17 @@ import haxe.macro.Expr;
 @desc("Type check for member variables")
 class TypeCheck extends Check {
 
+	public var ignoreEnumAbstractValues:Bool;
+
+	public function new() {
+		super();
+		ignoreEnumAbstractValues = true;
+	}
+
 	override function actualRun() {
-		forEachField(function(f, _) {
+		forEachField(function(f, p) {
 			if (f.name == "new") return;
+			if (ignoreEnumAbstractValues && p == ENUM_ABSTRACT && f.access.indexOf(AStatic) == -1) return;
 			switch (f.kind) {
 				case FVar(t, e):
 					if (t == null) error(f.name, f.pos);
--- test/checks/TypeCheckTest.hx
+++ test/checks/TypeCheckTest.hx
@@ -5,13 +5,20 @@ import checkstyle.checks.TypeCheck;
 class TypeCheckTest extends CheckTestCase {
 
 	public function testClassVar() {
-		var msg = checkMessage(TypeTests.TEST1, new TypeCheck());
-		assertEquals(msg, 'Type not specified: _a');
+		assertMsg(new TypeCheck(), TypeTests.TEST1, 'Type not specified: _a');
 	}
 
 	public function testStaticClassVar() {
-		var msg = checkMessage(TypeTests.TEST2, new TypeCheck());
-		assertEquals(msg, 'Type not specified: A');
+		assertMsg(new TypeCheck(), TypeTests.TEST2, 'Type not specified: A');
+	}
+	
+	public function testEnumAbstract() {
+		assertMsg(new TypeCheck(), TypeTests.TEST3, '');
+		assertMsg(new TypeCheck(), TypeTests.TEST4, 'Type not specified: VALUE');
+
+		var check = new TypeCheck();
+		check.ignoreEnumAbstractValues = false;
+		assertMsg(check, TypeTests.TEST3, 'Type not specified: VALUE');
 	}
 }
 
@@ -27,5 +34,17 @@ class TypeTests {
 	public static inline var TEST2:String = "
 	class Test {
 		static inline var A = 1;
+	}";
+
+	public static inline var TEST3:String =
+	"@:enum
+	abstract Test(Int) {
+		var VALUE = 0;
+	}";
+
+	public static inline var TEST4:String =
+	"@:enum
+	abstract Test(Int) {
+		static inline var VALUE = 0;
 	}";
 }
