7a8dc1ed68c194dd3000c78f14d74ef47bd47d33
Gama11 <jensfischer95@gmail.com>
2016-03-04 16:35:24 +0100

    Report: separate styling from actual output

--- src/checkstyle/Report.hx
+++ src/checkstyle/Report.hx
@@ -1,6 +1,16 @@
 package checkstyle;
 
+import checkstyle.Report.Style;
 import sys.io.File;
+
+@:enum
+@SuppressWarnings('checkstyle:MemberName')
+abstract Style(Int) {
+	var BOLD = 1;
+	var RED = 91;
+	var BLUE = 94;
+	var MAGENTA = 95;
+}
 
 class Report {
 
@@ -20,7 +30,7 @@ class Report {
 		for (node in staticAnalysisXML.firstElement().elementsNamed("file")) {
 			var fileNode:Xml = node;
 			fileName = fileNode.get("name").split("&#x2F;").join(".");
-			if (fileNode.elementsNamed("error").hasNext()) Sys.println("\033[1mCLASS: " + fileName + "\033[0m");
+			if (fileNode.elementsNamed("error").hasNext()) Sys.println(styleText("CLASS: " + fileName, Style.BOLD));
 			for (error in fileNode.elementsNamed("error")) {
 				var errorNode:Xml = error;
 				var line = errorNode.get("line");
@@ -28,22 +38,33 @@ class Report {
 				switch (errorNode.get("severity")) {
 					case "error":
 						errors++;
-						printMessage(91, "Error", line, message);
+						printMessage(Style.RED, "Error", line, message);
 					case "warning":
 						warnings++;
-						printMessage(95, "Warning", line, message);
+						printMessage(Style.MAGENTA, "Warning", line, message);
 					case "info":
 						infos++;
-						printMessage(94, "Info", line, message);
+						printMessage(Style.BLUE, "Info", line, message);
 				}
 				total++;
 			}
 		}
 
-		Sys.println("\033[1m\nTotal Issues: " + total + " (\033[0m\033[91mErrors: " + errors + "\033[0m, \033[95mWarnings: " + warnings + "\033[0m, \033[94mInfos: " + infos + ")\n" + "\033[0m");
+		Sys.println(
+			styleText("\nTotal Issues: " + total + " (", Style.BOLD) +
+			styleText("Errors: " + errors, Style.RED) +
+			styleText(", ", Style.BOLD) +
+			styleText("Warnings: " + warnings, Style.MAGENTA) +
+			styleText(", ", Style.BOLD) +
+			styleText("Infos: " + infos, Style.BLUE) +
+			styleText(")", Style.BOLD));
 	}
 
-	function printMessage(color:Int, type:String, line:String, message:String) {
-		Sys.println("\t\033[" + color + "m" + type + ": LINE - " + line + ": " + message + "\033[0m");
+	function printMessage(style:Style, type:String, line:String, message:String) {
+		Sys.println("\t" + styleText(Std.string(type + ": LINE - " + line + ": " + message), style));
+	}
+
+	function styleText(s:String, style:Style):String {
+		return '\033[${style}m${s}\033[0m';
 	}
 }
