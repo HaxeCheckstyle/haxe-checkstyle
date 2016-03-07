8170bcaa9d6d36fd2c92220a7ee1fd194e3e8fdf
Adi <adiinteractive@gmail.com>
2016-03-07 15:43:58 +0000

    new check HideUtilityClassConstructor

---
+++ src/checkstyle/checks/design/HideUtilityClassConstructorCheck.hx
@@ -0,0 +1,45 @@
+package checkstyle.checks.design;
+
+import checkstyle.token.TokenTree;
+import haxe.macro.Expr;
+import checkstyle.LintMessage.SeverityLevel;
+
+@name("HideUtilityClassConstructor")
+@desc("Makes sure that utility classes (classes that contain only static methods or fields in their API) do not have a constructor")
+class HideUtilityClassConstructorCheck extends Check {
+
+	public function new() {
+		super(TOKEN);
+	}
+
+	override function actualRun() {
+		var root:TokenTree = checker.getTokenTree();
+		var classes:Array<TokenTree> = root.filter([Kwd(KwdClass)], ALL);
+		for (cls in classes) {
+			var acceptableTokens:Array<TokenTree> = cls.filter([
+				Kwd(KwdFunction),
+				Kwd(KwdVar)
+			], ALL);
+
+			var haveConstructor:Bool = false;
+			var staticTokens:Int = 0;
+			var constructorPos = null;
+			for (token in acceptableTokens) {
+				if (token.filter([Kwd(KwdNew)], FIRST).length > 0) {
+					haveConstructor = true;
+					constructorPos = token.pos;
+					continue;
+				}
+
+				if (token.filter([Kwd(KwdStatic)], FIRST).length > 0) {
+					staticTokens++;
+					continue;
+				}
+			}
+
+			if (haveConstructor && acceptableTokens.length == staticTokens + 1) {
+				logPos("Utility classes should not have a constructor", constructorPos, severity);
+			}
+		}
+	}
+}
