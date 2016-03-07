f0bc58a76bc4172f0229325b142c04873f29aad9
Gama11 <jensfischer95@gmail.com>
2016-03-07 17:27:46 +0100

    Basic define combinations test for MemberNameCheckTest

--- test/checks/CheckTestCase.hx
+++ test/checks/CheckTestCase.hx
@@ -17,30 +17,31 @@ class CheckTestCase extends haxe.unit.TestCase {
 
 	override public function setup() {}
 
-	function assertMsg(check:Check, testCase:String, expected:String, ?pos:PosInfos) {
+	function assertMsg(check:Check, testCase:String, expected:String, ?defines:Array<Array<String>>, ?pos:PosInfos) {
 		var re = ~/abstractAndClass ([a-zA-Z0-9]*)/g;
 		if (re.match(testCase)) {
 			actualAssertMsg(check, re.replace(testCase, "class $1"), expected, pos);
 			actualAssertMsg(check, re.replace(testCase, "abstract $1(Int)"), expected, pos);
 		}
-		else actualAssertMsg(check, testCase, expected, pos);
+		else actualAssertMsg(check, testCase, expected, defines, pos);
 	}
 
 	function assertNoMsg(check:Check, testCase:String, ?pos:PosInfos) {
-		assertMsg(check, testCase, '', pos);
+		assertMsg(check, testCase, '', null, pos);
 	}
 
-	function actualAssertMsg(check:Check, testCase:String, expected:String, ?pos:PosInfos) {
-		var msg = checkMessage(testCase, check);
+	function actualAssertMsg(check:Check, testCase:String, expected:String, ?defines:Array<Array<String>>, ?pos:PosInfos) {
+		var msg = checkMessage(testCase, check, defines);
 		assertEquals(expected, msg, pos);
 	}
 
-	function checkMessage(src, check):String {
+	function checkMessage(src:String, check:Check, defines:Array<Array<String>>):String {
 		// a fresh Checker and Reporter for every checkMessage
 		// to allow multiple independant checkMessage calls in a single test
 		checker = new Checker();
 		reporter = new TestReporter();
 
+		if (defines != null) checker.defineCombinations = defines;
 		checker.addCheck(check);
 		checker.addReporter(reporter);
 		checker.process([{name:FILE_NAME, content:src, index:0}]);
