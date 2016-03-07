ba75c31771a26fea280c4a02b850e83efa691476
Adi <adiinteractive@gmail.com>
2016-03-07 13:29:40 +0000

    more tests

--- test/checks/coding/SimplifyBooleanReturnCheckTest.hx
+++ test/checks/coding/SimplifyBooleanReturnCheckTest.hx
@@ -9,15 +9,39 @@ class SimplifyBooleanReturnCheckTest extends CheckTestCase {
 	public function testWrongExpression() {
 		assertMsg(new SimplifyBooleanReturnCheck(), SimplifyBooleanReturnCheckTests.TEST1, MSG_SIMPLIFY);
 	}
+
+	public function testCorrectExpression() {
+		assertNoMsg(new SimplifyBooleanReturnCheck(), SimplifyBooleanReturnCheckTests.TEST2);
+	}
+
+	public function testOnlyIfExpression() {
+		assertNoMsg(new SimplifyBooleanReturnCheck(), SimplifyBooleanReturnCheckTests.TEST3);
+	}
 }
 
 class SimplifyBooleanReturnCheckTests {
 	public static inline var TEST1:String = "
-	class Test {
+	abstractAndClass Test {
 		function test() {
 			var a = (10 / 5 == 2);
 			if (a) return false;
 			else return true;
+		}
+	}";
+
+	public static inline var TEST2:String = "
+	abstractAndClass Test {
+		function test() {
+			var a = (10 / 5 == 2);
+			return a;
+		}
+	}";
+
+	public static inline var TEST3:String = "
+	abstractAndClass Test {
+		function test() {
+			var a = (10 / 5 == 2);
+			if (a) return false;
 		}
 	}";
 }
