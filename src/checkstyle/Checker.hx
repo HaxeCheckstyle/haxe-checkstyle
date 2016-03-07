af795f84c08d860cf781418f02638a0fd263707f
Gama11 <jensfischer95@gmail.com>
2016-03-07 22:02:22 +0100

    Checker: don't duplicate process()

--- src/checkstyle/Checker.hx
+++ src/checkstyle/Checker.hx
@@ -135,31 +135,24 @@ class Checker {
 		return parser.parse();
 	}
 
-#if hxtelemetry
 	public function process(files:Array<LintFile>) {
+		var advanceFrame = function() {};
+		#if hxtelemetry
 		var hxt = new hxtelemetry.HxTelemetry();
+		advanceFrame = function() hxt.advance_frame();
+		#end
+
 		for (reporter in reporters) reporter.start();
 		for (lintFile in files) {
 			loadFileContent(lintFile);
 			if (createContext(lintFile)) run();
 			unloadFileContent(lintFile);
-			hxt.advance_frame();
+			advanceFrame();
 		}
-		hxt.advance_frame();
+		advanceFrame();
 		for (reporter in reporters) reporter.finish();
-		hxt.advance_frame();
+		advanceFrame();
 	}
-#else
-	public function process(files:Array<LintFile>) {
-		for (reporter in reporters) reporter.start();
-		for (lintFile in files) {
-			loadFileContent(lintFile);
-			if (createContext(lintFile)) run();
-			unloadFileContent(lintFile);
-		}
-		for (reporter in reporters) reporter.finish();
-	}
-#end
 
 	function loadFileContent(lintFile:LintFile) {
 		// unittests set content before running Checker
