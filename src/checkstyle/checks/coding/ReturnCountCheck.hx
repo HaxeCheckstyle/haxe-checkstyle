59a656471e158dfe839535b64212a85ecee0702b
Adi <adiinteractive@gmail.com>
2016-03-07 11:08:55 +0000

    added ignoreFormat option to the check

--- src/checkstyle/checks/coding/ReturnCountCheck.hx
+++ src/checkstyle/checks/coding/ReturnCountCheck.hx
@@ -10,16 +10,24 @@ import checkstyle.LintMessage.SeverityLevel;
 class ReturnCountCheck extends Check {
 
 	public var max:Int;
+	public var ignoreFormat:String;
 
 	public function new() {
 		super();
 		max = 2;
+		ignoreFormat = "^$";
 	}
 
 	override function actualRun() {
+		var ignoreFormatRE:EReg  = new EReg(ignoreFormat, "");
 		var root:TokenTree = checker.getTokenTree();
 		var functions = root.filter([Kwd(KwdFunction)], ALL);
 		for (fn in functions) {
+			switch (fn.getFirstChild().tok) {
+				case Const(CIdent(name)):
+					if (ignoreFormatRE.match(name)) continue;
+				default:
+			}
 			if (isPosSuppressed(fn.pos)) continue;
 			if (!fn.hasChilds()) throw "function has invalid structure!";
 			var returns = fn.filter([Kwd(KwdReturn)], ALL);
