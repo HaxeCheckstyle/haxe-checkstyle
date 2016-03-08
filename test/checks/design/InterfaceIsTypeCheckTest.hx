b9b187b0e20c5793d5331cbb8bf5f026b18665c7
Adi <adiinteractive@gmail.com>
2016-03-08 14:59:21 +0000

    cleanup

--- test/checks/design/InterfaceIsTypeCheckTest.hx
+++ test/checks/design/InterfaceIsTypeCheckTest.hx
@@ -5,21 +5,21 @@ import checkstyle.checks.design.InterfaceIsTypeCheck;
 class InterfaceIsTypeCheckTest extends CheckTestCase<InterfaceIsTypeCheckTests> {
 
 	public function testWithJustVars() {
-		assertMsg(new InterfaceIsTypeCheck(), InterfaceIsTypeCheckTests.TEST1, "Interfaces should describe a type and hence have methods");
+		assertMsg(new InterfaceIsTypeCheck(), TEST1, "Interfaces should describe a type and hence have methods");
 	}
 
 	public function testMarkerInterface() {
-		assertNoMsg(new InterfaceIsTypeCheck(), InterfaceIsTypeCheckTests.TEST2);
+		assertNoMsg(new InterfaceIsTypeCheck(), TEST2);
 	}
 
 	public function testNoMarkerInterface() {
 		var chk = new InterfaceIsTypeCheck();
 		chk.allowMarkerInterfaces = false;
-		assertMsg(chk, InterfaceIsTypeCheckTests.TEST2, "Interfaces should describe a type and hence have methods");
+		assertMsg(chk, TEST2, "Interfaces should describe a type and hence have methods");
 	}
 
 	public function testCorrectInterface() {
-		assertNoMsg(new InterfaceIsTypeCheck(), InterfaceIsTypeCheckTests.TEST3);
+		assertNoMsg(new InterfaceIsTypeCheck(), TEST3);
 	}
 }
 
