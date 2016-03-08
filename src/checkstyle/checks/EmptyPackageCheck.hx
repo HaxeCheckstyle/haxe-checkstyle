8adebfde812018ef9bfaed90d6e7d5aa8f34c96e
Gama11 <jensfischer95@gmail.com>
2016-03-08 13:49:10 +0100

    EmptyPackageCheck: add an enforceEmptyPackage option

--- src/checkstyle/checks/EmptyPackageCheck.hx
+++ src/checkstyle/checks/EmptyPackageCheck.hx
@@ -8,13 +8,24 @@ import checkstyle.LintMessage.SeverityLevel;
 @desc("Checks for empty package names")
 class EmptyPackageCheck extends Check {
 
+	public var enforceEmptyPackage:Bool;
+
 	public function new() {
 		super(TOKEN);
+		enforceEmptyPackage = false;
 	}
 
 	override function actualRun() {
 		var root:TokenTree = checker.getTokenTree();
-		checkPackageNames(root.filter([Kwd(KwdPackage)], ALL));
+		var packageTokens = root.filter([Kwd(KwdPackage)], ALL);
+		if (enforceEmptyPackage) {
+			if (packageTokens.length == 0) {
+				log("Missing package declaration", 1, 0, 0, severity);
+			}
+		}
+		else {
+			checkPackageNames(packageTokens);
+		}
 	}
 
 	function checkPackageNames(entries:Array<TokenTree>) {
--- test/checks/EmptyPackageCheckTest.hx
+++ test/checks/EmptyPackageCheckTest.hx
@@ -6,10 +6,20 @@ class EmptyPackageCheckTest extends CheckTestCase<EmptyPackageCheckTests> {
 
 	public function testEmptyPackage() {
 		assertMsg(new EmptyPackageCheck(), TEST1, "Found empty package");
+		assertNoMsg(new EmptyPackageCheck(), TEST3);
 	}
 
 	public function testCorrectPackage() {
 		assertNoMsg(new EmptyPackageCheck(), TEST2);
+	}
+
+	public function testEnforceEmptyPackage() {
+		var check = new EmptyPackageCheck();
+		check.enforceEmptyPackage = true;
+
+		assertNoMsg(check, TEST1);
+		assertNoMsg(check, TEST2);
+		assertMsg(check, TEST3, "Missing package declaration");
 	}
 }
 
@@ -25,6 +35,11 @@ abstract EmptyPackageCheckTests(String) to String {
 	var TEST2 = "
 	package checks.test;
 
+	class Test {
+		public function new() {}
+	}";
+
+	var TEST3 = "
 	class Test {
 		public function new() {}
 	}";
