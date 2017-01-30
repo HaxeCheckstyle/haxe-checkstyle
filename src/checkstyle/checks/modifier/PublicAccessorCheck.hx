2558d9d3752632cf9ab2933103ff8b4e65cdd32f
Gama11 <jensfischer95@gmail.com>
2017-01-30 22:44:34 +0100

    Add PublicAccessorCheck, closes #243

---
+++ src/checkstyle/checks/modifier/PublicAccessorCheck.hx
@@ -0,0 +1,33 @@
+package checkstyle.checks.modifier;
+
+import haxe.macro.Expr;
+
+using checkstyle.utils.ArrayUtils;
+using checkstyle.utils.FieldUtils;
+using StringTools;
+
+@name("PublicAccessor")
+@desc("Checks for public accessors.")
+class PublicAccessorCheck extends Check {
+
+	public function new() {
+		super(AST);
+		categories = [Category.STYLE, Category.CLARITY];
+		points = 1;
+	}
+
+	override function actualRun() {
+		forEachField(checkField);
+	}
+
+	function checkField(f:Field, p:ParentType) {
+		if (!f.kind.match(FFun(_))) return;
+		if (!f.name.startsWith("set_") && !f.name.startsWith("get_")) return;
+
+		var isDefaultPrivate = f.isDefaultPrivate(p);
+		if (isDefaultPrivate && !f.access.contains(APublic)) return;
+		else if (!isDefaultPrivate && f.access.contains(APrivate)) return;
+
+		logPos("Accessor method should not be public", f.pos);
+	}
+}
