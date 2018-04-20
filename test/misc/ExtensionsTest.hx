530876c211c9687eca2ff83e25c05640e4dd5980
AlexHaxe <Alexander.Blum@gmail.com>
2018-04-21 00:25:47 +0200

    fixes #181

--- test/misc/ExtensionsTest.hx
+++ test/misc/ExtensionsTest.hx
@@ -1,19 +1,13 @@
 package misc;
 
 import checks.CheckTestCase;
-#if (haxeparser >= "3.3.0")
 import checkstyle.checks.whitespace.IndentationCharacterCheck;
-#end
 
 class ExtensionsTest extends CheckTestCase<ExtensionsTests> {
 
 	@Test
 	public function testExtensions() {
-		#if (haxeparser >= "3.3.0")
 		assertNoMsg(new IndentationCharacterCheck(), TEST1);
-		#else
-		assertTrue(true);
-		#end
 	}
 }
 
