cb55a415a3540f196db1a4c37dd0d9d54a4b949e
AlexHaxe <Alexander.Blum@gmail.com>
2018-05-01 17:24:52 +0200

    removed import

--- src/checkstyle/utils/StringUtils.hx
+++ src/checkstyle/utils/StringUtils.hx
@@ -1,14 +1,12 @@
 package checkstyle.utils;
 
-import byte.ByteData;
-
 class StringUtils {
 
 	public static inline function contains(s:String, c:String):Bool {
 		return s.indexOf(c) != -1;
 	}
 
-	public static function isStringInterpolation(s:String, fileContent:ByteData, pos:Position):Bool {
+	public static function isStringInterpolation(s:String, fileContent:byte.ByteData, pos:Position):Bool {
 		var code:Bytes = cast fileContent;
 		var quote:String = code.sub(pos.min, 1).toString();
 		if (quote != "'") return false;
