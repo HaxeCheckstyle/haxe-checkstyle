c8a4ac813143f757aae59312572fc6ca9c430258
AlexHaxe <Alexander.Blum@gmail.com>
2018-06-26 22:46:34 +0200

    new checks RedundantAccessMeta + RedundantAllowMeta, fixes #193 (#445)
    * new checks RedundantAccessMeta + RedundantAllowMeta, fixes #193

---
+++ src/checkstyle/checks/meta/RedundantAccessMetaCheck.hx
@@ -0,0 +1,12 @@
+package checkstyle.checks.meta;
+
+/**
+	Checks for redundant @:access metadata
+ **/
+@name("RedundantAccessMeta")
+@desc("Checks for redundant @:access metadata")
+class RedundantAccessMetaCheck extends RedundantAccessMetaBase {
+	public function new() {
+		super("access");
+	}
+}
