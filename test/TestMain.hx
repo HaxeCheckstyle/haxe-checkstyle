8380404e5afe2356868b6a772833722bad7fedf7
AlexHaxe <Alexander.Blum@gmail.com>
2018-11-23 18:43:32 +0100

    refactored coverage reporting (#462)
    * refactored coverage reporting

--- test/TestMain.hx
+++ test/TestMain.hx
@@ -1,12 +1,11 @@
 import haxe.EntryPoint;
-import haxe.Json;
-import sys.io.File;
-import sys.io.FileOutput;
 import massive.munit.TestRunner;
 import mcover.coverage.munit.client.MCoverPrintClient;
-import mcover.coverage.data.CoverageResult;
-import mcover.coverage.data.Statement;
-import mcover.coverage.data.Branch;
+#if codecov_json
+import mcover.coverage.client.CodecovJsonPrintClient;
+#else
+import mcover.coverage.client.LcovPrintClient;
+#end
 import mcover.coverage.MCoverage;
 
 using StringTools;
@@ -16,6 +15,11 @@ class TestMain {
 		var suites:Array<Class<massive.munit.TestSuite>> = [TestSuite];
 
 		var client:MCoverPrintClient = new MCoverPrintClient();
+		#if codecov_json
+		MCoverage.getLogger().addClient(new CodecovJsonPrintClient());
+		#else
+		MCoverage.getLogger().addClient(new LcovPrintClient("Formatter Unittests"));
+		#end
 		var runner:TestRunner = new TestRunner(client);
 		runner.completionHandler = completionHandler;
 		#if (neko || cpp || hl)
@@ -27,7 +31,7 @@ class TestMain {
 	}
 
 	function completionHandler(success:Bool) {
-		setupCoverageReport();
+		// setupCoverageReport();
 		#if eval
 		if (!success) {
 			Sys.exit(1);
@@ -35,37 +39,6 @@ class TestMain {
 		#else
 		Sys.exit(success ? 0 : 1);
 		#end
-	}
-
-	function setupCoverageReport() {
-		var report = {coverage: {}};
-		var classes = MCoverage.getLogger().coverage.getClasses();
-		for (cls in classes) {
-			var coverageData:Array<LineCoverageResult> = [null];
-			var results:CoverageResult = cls.getResults();
-			for (i in 1...results.l) coverageData[i] = 1;
-			var c = cls.name.replace(".", "/") + ".hx";
-
-			var missingStatements:Array<Statement> = cls.getMissingStatements();
-			for (stmt in missingStatements) {
-				for (line in stmt.lines) coverageData[line + 1] = 0;
-			}
-			var missingBranches:Array<Branch> = cls.getMissingBranches();
-			for (branch in missingBranches) {
-				if (branch.lines.length <= 0) continue;
-				var count:Int = 0;
-				if (branch.trueCount > 0) count++;
-				if (branch.falseCount > 0) count++;
-				var line:Int = branch.lines[branch.lines.length - 1];
-				coverageData[line] = count + "/2";
-			}
-
-			Reflect.setField(report.coverage, c, coverageData);
-		}
-
-		var file:FileOutput = File.write("coverage.json");
-		file.writeString(Json.stringify(report));
-		file.close();
 	}
 
 	static function main() {
