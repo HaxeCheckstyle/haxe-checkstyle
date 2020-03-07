533959a3acd5082aff7cccf885cefc86b9d5e201
AlexHaxe <Alexander.Blum@gmail.com>
2020-03-07 15:11:38 +0100

    use npx inside fixHaxeshim.sh

--- fixHaxeshim.sh
+++ fixHaxeshim.sh
@@ -2,7 +2,7 @@
 
 git clone --depth 1 -b fix_-x https://github.com/AlexHaxe/haxeshim.git
 
-(cd haxeshim; npm i lix; lix download; haxe all.hxml)
+(cd haxeshim; npm i lix; npx lix download; npx haxe all.hxml)
 
 cp haxeshim/bin/*.js node_modules/lix/bin
 
