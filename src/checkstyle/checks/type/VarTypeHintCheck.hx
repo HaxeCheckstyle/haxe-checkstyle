3d1d8cdf7b3586394944be09bfe44b9a2be4111e
AlexHaxe <Alexander.Blum@gmail.com>
2020-06-27 22:53:39 +0200

    added VarTypeHint check to replace EnforceVarTypeHint

---
+++ src/checkstyle/checks/type/VarTypeHintCheck.hx
@@ -0,0 +1,150 @@
+package checkstyle.checks.type;
+
+/**
+	Checks if type is specified or not for member variables.
+**/
+@name("VarTypeHint", "EnforceVarTypeHint")
+@desc("Checks type hints of variables.")
+class VarTypeHintCheck extends Check {
+	/**
+		policy for type hints on var and final
+	**/
+	public var typeHintPolicy:VarTypeHintPolicy;
+
+	/**
+		ignores fields inside abstract enums
+	**/
+	public var ignoreEnumAbstractValues:Bool;
+
+	public function new() {
+		super(TOKEN);
+		typeHintPolicy = INFER_NEW_OR_CONST;
+		ignoreEnumAbstractValues = true;
+		categories = [CLARITY, BUG_RISK];
+	}
+
+	override function actualRun() {
+		var root:TokenTree = checker.getTokenTree();
+		var varList:Array<TokenTree> = root.filterCallback(function(token:TokenTree, index:Int):FilterResult {
+			return switch (token.tok) {
+				#if haxe4
+				case Kwd(KwdFinal):
+					FoundSkipSubtree;
+				#else
+				case Const(CIdent("final")):
+					FoundSkipSubtree;
+				#end
+				case Kwd(KwdVar):
+					FoundSkipSubtree;
+				default:
+					return GoDeeper;
+			}
+		});
+		for (v in varList) {
+			checkVar(v);
+		}
+	}
+
+	function checkVar(token:TokenTree) {
+		if (isPosSuppressed(token.pos)) return;
+		if (!token.hasChildren()) return;
+
+		for (child in token.children) {
+			checkVarName(child);
+		}
+	}
+
+	function checkVarName(name:TokenTree) {
+		if (ignoreEnumAbstractValues && isEnumAbstractValue(name)) return;
+		switch (name.tok) {
+			case Root:
+				return;
+			case Question:
+				checkVarName(name.getFirstChild());
+				return;
+			default:
+		}
+		var colon:Null<TokenTree> = name.access().firstOf(DblDot).token;
+		var opAssign:Null<TokenTree> = name.access().firstOf(Binop(OpAssign)).token;
+		var hasNewOrConst:Bool = detectNewOrConst(opAssign);
+		switch (typeHintPolicy) {
+			case ENFORCE_ALL:
+				if (colon == null) needsTypeHint(name.toString(), name.pos);
+
+			case INFER_NEW_OR_CONST if (opAssign == null):
+				if (colon != null) return;
+				needsTypeHint(name.toString(), name.pos);
+			case INFER_NEW_OR_CONST if (colon == null):
+				if (hasNewOrConst) return;
+				needsTypeHint(name.toString(), name.pos);
+			case INFER_NEW_OR_CONST:
+				if (!hasNewOrConst) return;
+				noTypeHintNeeded(name.toString(), name.pos);
+
+			case INFER_ALL if (opAssign == null):
+				if (colon != null) return;
+				needsTypeHint(name.toString(), name.pos);
+			case INFER_ALL:
+				if (colon != null) noTypeHintNeeded(name.toString(), name.pos);
+		}
+	}
+
+	function detectNewOrConst(token:TokenTree):Bool {
+		if (token == null) return false;
+		for (child in token.children) {
+			switch (child.tok) {
+				case Const(CString(_)) | Const(CInt(_)) | Const(CFloat(_)) | Const(CRegexp(_)):
+					return true;
+				case Kwd(KwdTrue) | Kwd(KwdFalse):
+					return true;
+				case Kwd(KwdNew):
+					return true;
+				case Semicolon:
+					return false;
+				default:
+			}
+		}
+		return false;
+	}
+
+	function isEnumAbstractValue(token:TokenTree):Bool {
+		var parent:Null<TokenTree> = token.parent;
+		while ((parent != null) && (parent.tok != Root)) {
+			switch (parent.tok) {
+				case Kwd(KwdAbstract):
+					return TokenTreeCheckUtils.isTypeEnumAbstract(parent);
+				case Kwd(KwdInterface):
+					return false;
+				case Kwd(KwdClass):
+					return false;
+				case Kwd(KwdEnum):
+					return false;
+				case Kwd(KwdTypedef):
+					return false;
+				default:
+			}
+			parent = parent.parent;
+		}
+		return false;
+	}
+
+	function needsTypeHint(name:String, pos:Position) {
+		logPos('"${name}" should have a type hint', pos);
+	}
+
+	function noTypeHintNeeded(name:String, pos:Position) {
+		logPos('"${name}" type hint not needed', pos);
+	}
+}
+
+/**
+	enforce_all = var / final require a type hint
+	infer_new_or_const = var / final require a type hint unless you assign a number, a string or new <Object>
+	infer_all = var / final only require a type hint if you do not assign anything
+**/
+@:enum
+abstract VarTypeHintPolicy(String) {
+	var ENFORCE_ALL = "enforce_all";
+	var INFER_NEW_OR_CONST = "infer_new_or_const";
+	var INFER_ALL = "infer_all";
+}
---
+++ test/checkstyle/checks/type/VarTypeHintCheckTest.hx
@@ -0,0 +1,342 @@
+package checkstyle.checks.type;
+
+class VarTypeHintCheckTest extends CheckTestCase<VarTypeHintCheckTests> {
+	@Test
+	public function testEnforceTypeHints() {
+		var check = new VarTypeHintCheck();
+		check.typeHintPolicy = ENFORCE_ALL;
+		assertNoMsg(check, SUPPRESSED_LOCAL_VARS);
+
+		var messages:Array<String> = [for (t in [
+			 "test1",  "test2",  "test3",  "test6",  "test9",
+			"test10", "test11", "test12", "test13", "test14",
+			"test15", "test26", "test27", "test28", "test31",
+			"test32", "test33", "test34", "test39", "test41"
+		]) {
+			return '"$t" should have a type hint';
+		}];
+
+		assertMessages(check, LOCAL_VARS, messages);
+		#if haxe4
+		assertMessages(check, LOCAL_FINALS, messages);
+
+		messages = [for (t in ["test1", "test9", "test13", "test26", "test32", "test39"]) {
+			return '"$t" should have a type hint';
+		}];
+		assertMessages(check, MODULE_LEVEL_VARS, messages);
+		assertMessages(check, MODULE_LEVEL_FINALS, messages);
+		#end
+
+		assertNoMsg(check, ENUM);
+		assertMessages(check, TYPEDEF, ['"test0" should have a type hint', '"test1" should have a type hint']);
+		assertMessages(check, INTERFACE, ['"test0" should have a type hint']);
+	}
+
+	@Test
+	public function testInferConstAndNewTypeHints() {
+		var check = new VarTypeHintCheck();
+		check.typeHintPolicy = INFER_NEW_OR_CONST;
+		assertNoMsg(check, SUPPRESSED_LOCAL_VARS);
+
+		var messages:Array<String> = [for (t in ["test1", "test2", "test3", "test6", "test12"]) {
+			return '"$t" should have a type hint';
+		}];
+		messages = messages.concat([
+			for (t in ["test16", "test17", "test20", "test21", "test22", "test29", "test30"]) {
+				return '"$t" type hint not needed';
+			}
+		]);
+		messages = messages.concat([for (t in ["test32", "test33", "test34"]) {
+			return '"$t" should have a type hint';
+		}]);
+		messages.push('"test40" type hint not needed');
+
+		assertMessages(check, LOCAL_VARS, messages);
+		#if haxe4
+		assertMessages(check, LOCAL_FINALS, messages);
+
+		messages = [
+			'"test1" should have a type hint',
+			'"test16" type hint not needed',
+			'"test29" type hint not needed',
+			'"test32" should have a type hint'
+		];
+		assertMessages(check, MODULE_LEVEL_VARS, messages);
+		assertMessages(check, MODULE_LEVEL_FINALS, messages);
+		#end
+		assertNoMsg(check, ENUM);
+		assertMessages(check, TYPEDEF, ['"test0" should have a type hint', '"test1" should have a type hint']);
+		assertMessages(check, INTERFACE, ['"test0" should have a type hint']);
+	}
+
+	@Test
+	public function testInferAllTypeHints() {
+		var check = new VarTypeHintCheck();
+		check.typeHintPolicy = INFER_ALL;
+		assertNoMsg(check, SUPPRESSED_LOCAL_VARS);
+		var messages:Array<String> = [for (t in ["test1", "test2", "test3", "test6", "test12"]) {
+			return '"$t" should have a type hint';
+		}];
+		messages = messages.concat([for (t in [
+			"test16", "test17", "test20", "test21", "test22", "test29",
+			"test30", "test35", "test36", "test37", "test38", "test40"
+		]) {
+			return '"$t" type hint not needed';
+		}]);
+
+		assertMessages(check, LOCAL_VARS, messages);
+		#if haxe4
+		assertMessages(check, LOCAL_FINALS, messages);
+
+		messages = [
+			'"test1" should have a type hint',
+			'"test16" type hint not needed',
+			'"test29" type hint not needed',
+			'"test35" type hint not needed',
+			'"test38" type hint not needed'
+		];
+		assertMessages(check, MODULE_LEVEL_VARS, messages);
+		assertMessages(check, MODULE_LEVEL_FINALS, messages);
+		#end
+		assertNoMsg(check, ENUM);
+		assertMessages(check, TYPEDEF, ['"test0" should have a type hint', '"test1" should have a type hint']);
+		assertMessages(check, INTERFACE, ['"test0" should have a type hint']);
+	}
+
+	@Test
+	public function testCorrectTypeHints() {
+		var check = new VarTypeHintCheck();
+		check.typeHintPolicy = ENFORCE_ALL;
+
+		assertNoMsg(check, CORRECT_TYPE_HINTS);
+		assertNoMsg(check, ABSTRACT_ENUM);
+		assertNoMsg(check, ABSTRACT_ENUM2);
+		assertMsg(check, TYPEDEF_OLD, '"risk" should have a type hint');
+		#if haxe4
+		assertNoMsg(check, FINAL_FUNCTION);
+		assertMsg(check, FINAL_VAR, '"a" should have a type hint');
+
+		assertNoMsg(check, DOLLAR_VAR_CORRECT);
+		assertMsg(check, DOLLAR_VAR, '"$$a" should have a type hint');
+		#end
+
+		check.ignoreEnumAbstractValues = false;
+		assertMsg(check, ABSTRACT_ENUM, '"STYLE" should have a type hint');
+		assertMsg(check, ABSTRACT_ENUM2, '"STYLE" should have a type hint');
+	}
+}
+
+@:enum
+abstract VarTypeHintCheckTests(String) to String {
+	var MODULE_LEVEL_VARS = "
+	var test0:Int;
+	var test1;
+	var test9 = 10;
+	var test13 = '10';
+	var test16:Int = 10;
+
+	var test26 = new Test();
+	var test29:Test = new Test();
+	var test32 = call();
+	var test35:Test = call();
+	var test38:Test = /** **/ call();
+
+	var test39 = true;
+	";
+	var MODULE_LEVEL_FINALS = "
+	final test0:Int;
+	final test1;
+	final test9 = 10;
+	final test13 = '10';
+	final test16:Int = 10;
+
+	final test26 = new Test();
+	final test29:Test = new Test();
+	final test32 = call();
+	final test35:Test = call();
+	final test38:Test = /** **/ call();
+
+	final test39 = true;
+	";
+	var LOCAL_VARS = "
+	abstractAndClass Test {
+		function test() {
+			var test0:Int;
+			var test1, test2;
+			var test3, test4:Int;
+			var test5:Int, test6;
+			var test7:Int, test8:Int;
+
+			var test9 = 10;
+			var test10 = 10, test11 = '10';
+			var test12, test13 = '10';
+			var test14 = 10, test15 = '12';
+
+			var test16:Int = 10;
+			var test17:Int = 10, test18:String;
+			var test19:Int, test20:String = '14';
+			var test21:Int = 10, test22:String = '14';
+
+			// var @:meta test23:Int = 10;
+			// var @:meta test24:Int = 10, @:meta test25:Int = 10;
+
+			var test26 = new Test();
+			var test27 = new Test(), test28 = new Test2();
+
+			var test29:Test = new Test();
+			var test30:Test = new Test(), test31 = new Test2();
+
+			var test32 = call();
+			var test33 = call(), test34 = call();
+
+			var test35:Test = call();
+			var test36:Test = call(), test37:Test = call();
+
+			var test38:Test = /** **/ call();
+
+			var test39 = true;
+			var test40:Bool = true, test41 = false;
+		};
+	}
+	";
+	var LOCAL_FINALS = "
+	abstractAndClass Test {
+		function test() {
+			final test0:Int;
+			final test1, test2;
+			final test3, test4:Int;
+			final test5:Int, test6;
+			final test7:Int, test8:Int;
+
+			final test9 = 10;
+			final test10 = 10, test11 = '10';
+			final test12, test13 = '10';
+			final test14 = 10, test15 = '12';
+
+			final test16:Int = 10;
+			final test17:Int = 10, test18:String;
+			final test19:Int, test20:String = '14';
+			final test21:Int = 10, test22:String = '14';
+
+			// final @:meta test23:Int = 10;
+			// final @:meta test24:Int = 10, @:meta test25:Int = 10;
+
+			final test26 = new Test();
+			final test27 = new Test(), test28 = new Test2();
+
+			final test29:Test = new Test();
+			final test30:Test = new Test(), test31 = new Test2();
+
+			final test32 = call();
+			final test33 = call(), test34 = call();
+
+			final test35:Test = call();
+			final test36:Test = call(), test37:Test = call();
+
+			final test38:Test = /** **/ call();
+
+			final test39 = true;
+			final test40:Bool = true, test41 = false;
+		};
+	}
+	";
+	var SUPPRESSED_LOCAL_VARS = "
+	abstractAndClass Test {
+		@SuppressWarnings('checkstyle:VarTypeHint')
+		function test() {
+			var test0:Int;
+			var test1, test2;
+			var test3, test4:Int;
+			var test5:Int, test6;
+			var test7:Int, test8:Int;
+
+			var test9 = 10;
+			var test10 = 10, test11 = '10';
+			var test12, test13 = '10';
+			var test14 = 10, test15 = '12';
+
+			var test16:Int = 10;
+			var test17:Int = 10, test18:String;
+			var test19:Int, test20:String = '14';
+			var test21:Int = 10, test22:String = '14';
+
+			// var @:meta test23:Int = 10;
+			// var @:meta test24:Int = 10, @:meta test25:Int = 10;
+
+			var test26 = new Test();
+			var test27 = new Test(), test28 = new Test2();
+
+			var test29:Test = new Test();
+			var test30:Test = new Test(), test31 = new Test2();
+
+			var test32 = call();
+			var test33 = call(), test34 = call();
+
+			var test35:Test = call();
+			var test36:Test = call(), test37:Test = call();
+
+			var test38:Test = /** **/ call();
+		};
+	}
+	";
+	var ENUM = "
+	@:enum abstract Category(String) {
+		var STYLE = 'Style';
+	}
+	enum abstract Category(String) {
+		var STYLE = 'Style';
+	}
+	enum Category {
+		STYLE;
+	}
+	";
+	var TYPEDEF = "
+	typedef Category = {
+		var test0;
+		var ?test1;
+		var test2:Int;
+		var ?test3:Int;
+	}
+	";
+	var INTERFACE = "
+	interface Category {
+		var test0;
+		var test1:Int;
+	}
+	";
+	var CORRECT_TYPE_HINTS = "
+	abstractAndClass Test {
+		var a:Int;
+
+		@SuppressWarnings('checkstyle:VarTypeHint')
+		var _b;
+	}";
+	var ABSTRACT_ENUM = "
+	@:enum abstract Category(String) {
+		var STYLE = 'Style';
+	}";
+	var ABSTRACT_ENUM2 = "
+	enum abstract Category(String) {
+		var STYLE = 'Style';
+	}";
+	var TYPEDEF_OLD = "
+	typedef Category = {
+		var risk;
+	}";
+	var FINAL_VAR = "
+	abstractAndClass Test {
+		final a;
+	}";
+	var FINAL_FUNCTION = "
+	abstractAndClass Test {
+		final function test() {};
+	}";
+	var DOLLAR_VAR = "
+	abstractAndClass Test {
+		var $a;
+	}";
+	var DOLLAR_VAR_CORRECT = "
+	abstractAndClass Test {
+		var $a:Int;
+	}";
+}
